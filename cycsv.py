b20d0098405c2f623f238c67fa1e42a1e21082a6
Russell Nibbelink <russell.n@berkeley.edu>
2014-10-10 17:55:21 -0700

    interface

---
+++ cycsv.py
@@ -0,0 +1,31 @@
+import sqlite3
+def get_input():
+    return
+
+def connect_to_sqlite(sqlite_name): 
+    return
+
+def metrics():
+    """return the list of metrics list/dictionary tbd"""
+    return
+
+def get_important_data(metric):
+    """ returns dictionary/ndarray"""
+    return
+
+# class metric_calculator(object): # implement only if need be
+
+def calculate_metric(metric_name, metric_data): #whatever got spit out from the get_important_data
+    return
+
+def add_metric_val(val, met_name, yr):
+    return
+
+def add_column_to_table(metric_name, values):
+    return
+
+def create_csv(name):
+    return
+
+def create_new_csv(mydata):
+    return
