32257a0fe7a07c56fc23e1011e8f83131d72bc00
Russell Nibbelink <russell.n@berkeley.edu>
2014-12-12 13:48:49 -0800

    metrics added

--- y.py
+++ y.py
@@ -29,9 +29,10 @@ def make_sort_list(metric, table, order,filter_category, filter_metric):
     return mlist
 
 def make_raw_list(metric, table, order,filter_category, filter_list):
-    #make_sort_list('TransactionId', 'Transactions', 'Time', 'SenderId', 24)
     mlist = []
     i = 0
+    if (len(filter_list) == 0): 
+        return [[0, 0]]
     for row in c.execute('SELECT '+ metric +','+ filter_category + ' FROM ' + table + ' ORDER BY ' + order):
         if row[1] == filter_list[i][0]:
             mlist.append([row[0], filter_list[i][1]])
@@ -128,11 +129,12 @@ def csv_w(data, name):
 #get this to work -->
 #to do: not all data is sampled exactly every month. Keep track of time stamp to determine the data points for each year. (determine when a year has passed).
 
-### first metric
+###General Variables
 db_name = "cyclus200year.sqlite"
 connDB(db_name)
-
 sim_length = 200;
+
+### uranium mined
 
 metric_name = "Uranium Mined"
 metric_cat = "ResourceId"
@@ -156,7 +158,119 @@ csv_name = "Uranium_Mined.csv"
 csv_w(out_data, csv_name)
 
 
-### 
+######------------ Electricity Generated ----------#######
+
+#### functions for electricity generated #####
+def IO_raw(metric, table, order,filter_category, filter_metric):
+    mlist = []
+    for row in c.execute('SELECT '+ metric + ", EnterTime, ExitTime" +','+ filter_category + ' FROM ' + table + ' ORDER BY ' + order):
+        if row[3] == filter_metric:
+            mlist.append([row[0], row[1], row[2]])
+    #print(mlist)
+    return mlist
+
+def IO_list(metric, table, order,filter_category, filter_list):
+    mlist = []
+    i = 0
+    if (len(filter_list) == 0): 
+        return [[0, 0, 0]]
+    for row in c.execute('SELECT '+ metric +','+ filter_category + ' FROM ' + table + ' ORDER BY ' + order):
+        if row[1] == filter_list[i][0]:
+            mlist.append([row[0], filter_list[i][1], filter_list[i][2]])
+            if i == len(filter_list)-1:
+                break
+            i+=1
+    return mlist
+
+def IO_time_sort(processed_data, year = 200):
+    lst = [0.0] * year
+    i = 0
+    for year in lst:
+        for arr in processed_data:
+            if arr[1] <= i*12 and arr[2] > i*12:
+                year += arr[0]
+        lst[i] = year*12/1000
+        i+=1
+    return lst
+
+lookup_list_LWR = IO_raw("AgentId", "Facilities", "AgentId", "Prototype", "LWR")
+lookup_list_SFR = IO_raw("AgentId", "Facilities", "AgentId", "Prototype", "SFR")
+lookup_list_MOX = IO_raw("AgentId", "Facilities", "AgentId", "Prototype", "MOX")
+
+lookup_list_LWR = IO_list("out_commod_cap", "AgentState_cycamore_BatchReactorInfo", "AgentId", "AgentId", lookup_list_LWR)
+lookup_list_SFR = IO_list("out_commod_cap", "AgentState_cycamore_BatchReactorInfo", "AgentId", "AgentId", lookup_list_SFR)
+lookup_list_MOX = IO_list("out_commod_cap", "AgentState_cycamore_BatchReactorInfo", "AgentId", "AgentId", lookup_list_MOX)
+
+LWR_out = IO_time_sort(lookup_list_LWR)
+SFR_out = IO_time_sort(lookup_list_SFR)
+MOX_out = IO_time_sort(lookup_list_MOX)
+
+LWR_csv_name = "LWR_Electicity_Generated.csv"
+csv_w(LWR_out, LWR_csv_name)
+
+SFR_csv_name = "SFR_Electricity_Generated.csv"
+csv_w(SFR_out, SFR_csv_name)
+
+MOX_csv_name = "MOX_Electricity_Generated.csv"
+csv_w(MOX_out, MOX_csv_name)
+
+########---- UNFULLFILLED DEMAND
+
+demand = [200]*200
+
+LWR_csv_name = "LWR_Unfulfilled_Demand.csv"
+csv_w([i - j for i, j in zip(demand, LWR_out)], LWR_csv_name)
+
+SFR_csv_name = "SFR_Unfulfilled_Demand.csv"
+csv_w([i - j for i, j in zip(demand, SFR_out)], SFR_csv_name)
+
+MOX_csv_name = "MOX_Unfulfilled_Demand.csv"
+csv_w([i - j for i, j in zip(demand, MOX_out)], MOX_csv_name)
+
+#######------ CAPACITY STARTED -----
+#if its what I think not hard at all
+
+#######------ CAPACITY ENDED ------
+#ditto
+
+########-------FUEL LOADED -------- ########
+
+LWR_fuel_loaded = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "fresh_lwr_fuel")
+LWR_fuel_loaded = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", LWR_fuel_loaded)
+LWR_FL_out = time_sort(LWR_fuel_loaded)
+LWR_FL_name = "LWR_Fuel_Loaded.csv"
+csv_w(LWR_FL_out, LWR_FL_name)
+
+SFR_fuel_loaded = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "fresh_sfr_fuel")
+SFR_fuel_loaded = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", SFR_fuel_loaded)
+SFR_FL_out = time_sort(SFR_fuel_loaded)
+SFR_FL_name = "SFR_Fuel_Loaded.csv"
+csv_w(SFR_FL_out, SFR_FL_name)
+
+MOX_fuel_loaded = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "fresh_mox_fuel")
+MOX_fuel_loaded = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", MOX_fuel_loaded)
+MOX_FL_out = time_sort(MOX_fuel_loaded)
+MOX_FL_name = "MOX_Fuel_Loaded.csv"
+csv_w(MOX_FL_out, MOX_FL_name)
+
+#########---------FUEL LOADED ------- #######
+LWR_UNF_rep = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "rep_lwr_tru")
+LWR_UNF_rep = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", LWR_UNF_rep)
+LWR_rep_out = time_sort(LWR_UNF_rep)
+LWR_rep_name = "LWR_Reprocessed.csv"
+csv_w(LWR_rep_out, LWR_rep_name)
+
+SFR_UNF_rep = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "rep_sfr_tru")
+SFR_UNF_rep = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", SFR_UNF_rep)
+SFR_rep_out = time_sort(SFR_UNF_rep)
+SFR_rep_name = "SFR_Reprocessed.csv"
+csv_w(SFR_rep_out, SFR_rep_name)
+
+MOX_UNF_rep = make_sort_list("ResourceId", "Transactions", "Time", "Commodity", "rep_mox_tru")
+MOX_UNF_rep = make_raw_list("Quantity", "Resources", "ResourceId", "ResourceId", mox_UNF_rep)
+MOX_rep_out = time_sort(MOX_UNF_rep)
+MOX_rep_name = "MOX_Reprocessed.csv"
+csv_w(MOX_rep_out, MOX_rep_name)
 
 
 
